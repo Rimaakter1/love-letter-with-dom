1d703c7857be981dcba86d8f35c3b0164e14a5aa
unknown <Rimaakter1>
2022-08-18 16:25:25 +0600

    first commit

---
+++ letter.js
@@ -0,0 +1,8 @@
+document.getElementById('yes').addEventListener(`click`, function () {
+    document.getElementById('msg').innerText = 'I LOVE YOU💝'
+
+})
+function loveLetter() {
+    document.getElementById('msg').innerText = 'I LOVE YOU SO MUCH JAMAI😘💖'
+
+}
